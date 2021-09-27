5ce69bc3793794c0f43c3c15f3ed9d7a4c0acaf5
FahrizalUsman <fahrizalu260601@gmail.com>
2021-09-27 22:36:55 +0700

    Upload Tugas

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1><i>Perkenalan</i></h1>";
+		echo "<br><b><i>Nama Saya Fahrizal Usman</br>
+			  Saya tinggal di daerah Rawamangun
+			  <br>Olahraga yang saya sukai adalah 
+			  Badminton<b><i></br>";
+	}
+}
