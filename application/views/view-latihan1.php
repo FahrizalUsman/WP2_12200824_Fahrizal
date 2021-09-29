cd83407282a587479cf4559501d65294b5d57a8a
FahrizalUsman <fahrizalu260601@gmail.com>
2021-09-29 07:32:00 +0700

    Upload Tugas

--- application/views/view-latihan1.php
+++ application/views/view-latihan1.php
@@ -3,9 +3,9 @@
  <title>Latihan 1</title>
 </head>
 <body>
- Halo Kawan.. Yuk kita belajar web programming..!!!<br>
- Nilai 1 = <?= $nilai1; ?>
- Nilai 2 = <?= $nilai2; ?>
+ <br><b>Halo Kawan</b>.. Yuk kita belajar web programming..!!!</br>
+ <i>Nilai 1</i> = <?= $nilai1; ?>
+ <br><i>Nilai 2</i> = <?= $nilai2; ?></br>
  ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
 $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
 </body>
